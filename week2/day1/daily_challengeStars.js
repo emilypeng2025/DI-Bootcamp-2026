5350a7a9bd347a263fec194a2b99c6a9831d2367
emilypeng2025 <emilyjwpeng@gmail.com>
2026-02-23 00:12:36 +0200

    dailychallenge 2

---
+++ week2/day1/daily_challengeStars.js
@@ -0,0 +1,20 @@
+// What You will learn :
+// For loops and nested for loops
+
+
+// Instructions
+// Write a JavaScript program that recreates the pattern below.
+// Do this challenge twice: first by using one loop, then by using two nested for loops (Nested means one inside the other - check out this tutorial of nested loops).
+// Do this Daily Challenge by yourself, without looking at the answers on the internet.
+// *  
+// * *  
+// * * *  
+// * * * *  
+// * * * * *
+// * * * * * *
+
+let star = "";
+for (let i = 1; i<=6; i++){
+    star += "*";
+    console.log(star);
+}
